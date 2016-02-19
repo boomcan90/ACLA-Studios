52d7df59cb467edc1790ed596bc7658dc0d177e4
Lakshita23 <lakshita_lak95@yahoo.com>
2016-02-19 18:02:00 +0800

    Second Comit

--- core/src/com/aclastudios/spaceconquest/SpaceConquest.java
+++ core/src/com/aclastudios/spaceconquest/SpaceConquest.java
@@ -1,10 +1,11 @@
 package com.aclastudios.spaceconquest;
-
+//main class
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
 
 public class SpaceConquest extends ApplicationAdapter {
 	SpriteBatch batch;
