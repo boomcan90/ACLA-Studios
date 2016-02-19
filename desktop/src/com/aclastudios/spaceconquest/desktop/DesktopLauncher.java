2220a2a5e51780f2fbf63c5c14b7516205075833
Lakshita23 <lakshita_lak95@yahoo.com>
2016-02-19 17:47:04 +0800

    initial commit

---
+++ desktop/src/com/aclastudios/spaceconquest/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.aclastudios.spaceconquest.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.aclastudios.spaceconquest.SpaceConquest;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new SpaceConquest(), config);
+	}
+}
