ce6e751cf5083fbe4a0e72f1b1a25aaadbb9a12e
Amos <chooamos@yahoo.com.sg>
2016-03-01 18:51:05 +0800

    Added Joystick (with comments)

--- desktop/src/com/aclastudios/spaceconquest/desktop/DesktopLauncher.java
+++ desktop/src/com/aclastudios/spaceconquest/desktop/DesktopLauncher.java
@@ -8,9 +8,6 @@ import com.badlogic.gdx.graphics.glutils.ShaderProgram;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-//		config.useGL30 = true;
-//		ShaderProgram.prependVertexCode = "#version 140\n#define varying out\n#define attribute in\n";
-//		ShaderProgram.prependFragmentCode = "#version 140\n#define varying in\n#define texture2D texture\n#define gl_FragColor fragColor\nout vec4 fragColor;\n";
 		new LwjglApplication(new SpaceConquest(), config);
 	}
 }
