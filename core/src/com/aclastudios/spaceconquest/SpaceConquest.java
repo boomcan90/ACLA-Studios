ef7ace4981b57d7d42cb53f9fc1e86a54e677106
Lakshita23 <lakshita_lak95@yahoo.com>
2016-03-20 22:54:12 +0800

    resolving conflict

--- core/src/com/aclastudios/spaceconquest/SpaceConquest.java
+++ core/src/com/aclastudios/spaceconquest/SpaceConquest.java
@@ -10,7 +10,6 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 
 
-
 public class SpaceConquest extends Game {
 	public static final int V_WIDTH = 400; //Virtual screen width of the game
 	public static final int V_HEIGHT =208; //Virtual screen height of the game
