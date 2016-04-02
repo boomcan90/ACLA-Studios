52a0f2394f76eefcedddf6ce74d55f7d4b5f4e73
junhaochiew <junhaochiew@gmail.com>
2016-04-02 14:56:43 +0800

    character now gains 50 points upon killing enemy. item weight is now 1. Speed booster now added. Deposit methods now process item into resource into ammunition and jetpack time.

--- core/src/com/aclastudios/spaceconquest/Tools/WorldContactListener.java
+++ core/src/com/aclastudios/spaceconquest/Tools/WorldContactListener.java
@@ -30,28 +30,37 @@ public class WorldContactListener implements ContactListener {
 
         switch (cDef){
             case SpaceConquest.MAIN_CHARACTER_BIT | SpaceConquest.IRON_BIT:
-                if(fixA.getFilterData().categoryBits == SpaceConquest.IRON_BIT)
-                    ((Iron)fixA.getUserData()).use((MainCharacter) fixB.getUserData());
-                else
-                    ((Iron)fixB.getUserData()).use((MainCharacter) fixA.getUserData());
-                //Hud.addScore(1);
-                screen.increaseCharWeight(2);
+                if(fixA.getFilterData().categoryBits == SpaceConquest.IRON_BIT) {
+                    ((Iron) fixA.getUserData()).use((MainCharacter) fixB.getUserData());
+                    ((MainCharacter) fixB.getUserData()).addIron_count();
+                }
+                else {
+                    ((Iron) fixB.getUserData()).use((MainCharacter) fixA.getUserData());
+                    ((MainCharacter) fixA.getUserData()).addIron_count();
+                }
+                screen.increaseCharWeight(1);
                 break;
             case SpaceConquest.MAIN_CHARACTER_BIT | SpaceConquest.GUNPOWDER_BIT:
-                if(fixA.getFilterData().categoryBits == SpaceConquest.GUNPOWDER_BIT)
-                    ((GunPowder)fixA.getUserData()).use((MainCharacter) fixB.getUserData());
-                else
-                    ((GunPowder)fixB.getUserData()).use((MainCharacter) fixA.getUserData());
-                //Hud.addScore(1);
-                screen.increaseCharWeight(2);
+                if(fixA.getFilterData().categoryBits == SpaceConquest.GUNPOWDER_BIT) {
+                    ((GunPowder) fixA.getUserData()).use((MainCharacter) fixB.getUserData());
+                    ((MainCharacter) fixB.getUserData()).addGun_powder_count();
+                }
+                else {
+                    ((GunPowder) fixB.getUserData()).use((MainCharacter) fixA.getUserData());
+                    ((MainCharacter) fixA.getUserData()).addGun_powder_count();
+                }
+                screen.increaseCharWeight(1);
                 break;
             case SpaceConquest.MAIN_CHARACTER_BIT | SpaceConquest.OIL_BIT:
-                if(fixA.getFilterData().categoryBits == SpaceConquest.OIL_BIT)
-                    ((Oil)fixA.getUserData()).use((MainCharacter) fixB.getUserData());
-                else
-                    ((Oil)fixB.getUserData()).use((MainCharacter) fixA.getUserData());
-                //Hud.addScore(1);
-                screen.increaseCharWeight(2);
+                if(fixA.getFilterData().categoryBits == SpaceConquest.OIL_BIT) {
+                    ((Oil) fixA.getUserData()).use((MainCharacter) fixB.getUserData());
+                    ((MainCharacter) fixB.getUserData()).addOil_count();
+                }
+                else {
+                    ((Oil) fixB.getUserData()).use((MainCharacter) fixA.getUserData());
+                    ((MainCharacter) fixA.getUserData()).addOil_count();
+                }
+                screen.increaseCharWeight(1);
                 break;
             case SpaceConquest.MAIN_CHARACTER_BIT |SpaceConquest.STATION_BIT:
                 System.out.println("inside station");
@@ -62,26 +71,20 @@ public class WorldContactListener implements ContactListener {
                     screen.addscore("0",score);
                 }
                 break;
-//            case SpaceConquest.OBJECT_BIT| SpaceConquest.IRON_BIT:
-//                if(fixA.getFilterData().categoryBits == SpaceConquest.IRON_BIT)
-//                    ((Iron)fixA.getUserData()).use((MainCharacter) fixB.getUserData());
-//                else
-//                    ((Iron)fixB.getUserData()).use((MainCharacter) fixA.getUserData());
-//                break;
             case SpaceConquest.FIREBALL_BIT | SpaceConquest.OBSTACLE_BIT:
                 if(fixA.getFilterData().categoryBits == SpaceConquest.FIREBALL_BIT)
                     ((FireBall)fixA.getUserData()).setToDestroy();
                 else
                     ((FireBall)fixB.getUserData()).setToDestroy();
                 break;
-            case SpaceConquest.FIREBALL_BIT | SpaceConquest.CHARACTER_BIT:
-                if(fixA.getFilterData().categoryBits == SpaceConquest.FIREBALL_BIT) {
-                    ((FireBall) fixA.getUserData()).setToDestroy();
-                }
-                else {
-                    ((FireBall) fixB.getUserData()).setToDestroy();
-                }
-                break;
+//            case SpaceConquest.FIREBALL_BIT | SpaceConquest.CHARACTER_BIT:
+//                if(fixA.getFilterData().categoryBits == SpaceConquest.FIREBALL_BIT) {
+//                    ((FireBall) fixA.getUserData()).setToDestroy();
+//                }
+//                else {
+//                    ((FireBall) fixB.getUserData()).setToDestroy();
+//                }
+//                break;
             case SpaceConquest.FIREBALL_BIT | SpaceConquest.MAIN_CHARACTER_BIT:
                 if(fixA.getFilterData().categoryBits == SpaceConquest.FIREBALL_BIT) {
                     ((FireBall) fixA.getUserData()).setToDestroy();
@@ -90,6 +93,12 @@ public class WorldContactListener implements ContactListener {
                 else {
                     ((FireBall) fixB.getUserData()).setToDestroy();
                     ((MainCharacter) fixA.getUserData()).dead();
+                }
+                //if you die, your enemy gains 50 points
+                if (game.multiplayerSessionInfo.mId_num!=0) {
+                    game.playServices.MessagetoServer("Serverpoints:" + ((screen.getUserID() + (screen.getNumOfPlayers()/2)) / screen.getNumOfPlayers()) + ":" + 50);
+                } else {
+                    screen.addscore(Integer.toString((screen.getUserID() + 3) / 3), 50);
                 }
                 break;
         }
